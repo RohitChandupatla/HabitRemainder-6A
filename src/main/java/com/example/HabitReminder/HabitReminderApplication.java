238b50113252b5c58548664eda159b60880d056a
Rohitreddz <77590158+Rohitreddz@users.noreply.github.com>
2021-10-14 21:50:55 -0500

    Added the project

---
+++ src/main/java/com/example/HabitReminder/HabitReminderApplication.java
@@ -0,0 +1,13 @@
+package com.example.HabitReminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabitReminderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabitReminderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HabitReminder/HabitReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HabitReminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitReminderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
